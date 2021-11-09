cb9782e6f0d76f8c098884156aa55af143db2dcd
Juan_Carlos_V <juancarlos.vivero@utxicotepec.edu.mx>
2021-11-08 21:13:08 -0600

    subiendo a la nube

--- sw.js
+++ sw.js
@@ -25,78 +25,39 @@ self.addEventListener('install', (event) => {
 
 });
 
-/*
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((r) =>{
-            return r || fetch(e.request)
+
+self.addEventListener('activate', event => {
+  console.log('SW activado de juan Carlos');
+
+  const cacheAllowlist = [_cache];
+
+  event.waitUntil(//funciona como un awakw
+    caches.keys().then(cacheNames => {
+      return Promise.all(//promisetodas las promesas seran resultas 
+        cacheNames.map(cacheName => {// map con base a un arreglo comienza a extraer cada elemento de la lista
+          if (cacheAllowlist.indexOf(cacheName) === -1) {// si uno de los elementos de la vista  
+            return caches.delete(cacheName);
+          }
         })
-    );
-});*/
+      );
+    })
+  );
+});
 
 
-//1. cache Only
-/*self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request));
-    // la funcion open abre un cache pero si no existe lo crea ////Abrir o crear
-    //.match compara los archivos y responde
-    // event.respondWith(caches.match(event.request)); // vwerifica si la peticion es igual a algun recurso almacenado en cache
-    //cache only solo responde a tomar recursos del cache
-});*/
-/*self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request));
-});*/
-
-/*
-//2. Network Only
-// solo respondera de los datos que esten en Internet
-
-self.addEventListener('fetch', event => {
-    event.respondWith(event.request);
-})
-*/
 
 //3 Cache First
-self.addEventListener('fetch', (event) => {
-    event.responseWith(
-        caches.match(event.request).then((response) => {
+self.addEventListener('fetch', event => {
+    event.responseWith(caches.match(event.request).then((response) => {
             return response || fetch(event.request);
-
-        })
+    })
     );
     });
 
 
-    //Network Firts
-    /*self.addEventListener('fetch', (event) => {
-        event.respondWith(
-          fetch(event.request).catch(() => {
-            return caches.match(event.request);
-          })
-        );
-      });
-      */
+
   
                 
            
 
 
-
-    self.addEventListener('activate', event => {
-        console.log('SW activado de juan Carlos');
-      
-        const cacheAllowlist = [_cache];
-      
-        event.waitUntil(//funciona como un awakw
-          caches.keys().then(cacheNames => {
-            return Promise.all(//promisetodas las promesas seran resultas 
-              cacheNames.map(cacheName => {// map con base a un arreglo comienza a extraer cada elemento de la lista
-                if (cacheAllowlist.indexOf(cacheName) === -1) {// si uno de los elementos de la vista  
-                  return caches.delete(cacheName);
-                }
-              })
-            );
-          })
-        );
-      });
-      
